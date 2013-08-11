21e5aef8c16c17a3d2e1a8f1ca798909fcde2a0b
Seymour Mao <seymour.mao@gmail.com>
2013-08-11 21:22:55 +0800

    Update server.js

--- server.js
+++ server.js
@@ -61,61 +61,62 @@ app.get('/api/books', function (req, res) {
 //Insert a new book
 app.post('/api/books', function (req, res) {
   var book = new BookModel({
-  	title:req.body.title,
-  	author:req.body.author,
-  	releaseDate:req.body.releaseDate,
-  	coverImage: req.body.coverImage,
-  	keywords:req.body.keywords
+    title:req.body.title,
+    author:req.body.author,
+    releaseDate:req.body.releaseDate,
+    coverImage: req.body.coverImage,
+    keywords:req.body.keywords
   });
+
   book.save(function (err) {
-  	if (!err) {
-  		return console.log('created');
-  	} else {
-  		return console.log(err);
-  	}
-  });
-  return res.send(book);
+    if (!err) {
+      return console.log('created');
+    } else {
+      return console.log(err);
+    }
+    });
+    return res.send(book);
 });
 
 app.get('/api/books/:id', function(req, res){
   return BookModel.findById(req.params.id, function(err, book){
-  	if(!err){
-  		return res.send(book);
-  	} else {
-  		return console.log(err);
-  	}
+    if (!err){
+      return res.send(book);
+    } else {
+      return console.log(err);
+    }
   });
 });
 
 app.put('/api/books/:id', function(req, res){
-	console.log('Updating book ' + req.body.title);
-	return BookModel.findById(req.params.id, function(err, book){
-		book.title = req.body.title;
-		book.author = req.body.author;
-		book.releaseDate = req.body.releaseDate;
-		book.coverImage = req.body.coverImage,
-		book.keywords = req.body.keywords;
-		return book.save(function(err){
-			if(!err){
-				console.log('book updated');
-			} else {
-				console.log(err);
-			}
-		return res.send(book);
-		});
-	});
+  console.log('Updating book ' + req.body.title);
+  return BookModel.findById(req.params.id, function(err, book){
+    book.title = req.body.title;
+    book.author = req.body.author;
+    book.releaseDate = req.body.releaseDate;
+    book.coverImage = req.body.coverImage,
+    book.keywords = req.body.keywords;
+    return book.save(function(err){
+      if(!err){
+	console.log('book updated');
+      } else {
+	console.log(err);
+      }
+      return res.send(book);
+    });
+  });
 });
 
 app.delete('/api/books/:id', function(req, res){
-	console.log('Deleting book with id: ' + req.params.id);
-	return BookModel.findById(req.params.id, function(err, book){
-		return book.remove(function(err){
-			if(!err){
-				console.log('Book removed');
-				return res.send('');
-			} else {
-				console.log(err);
-			}
-		});
-	});
+  console.log('Deleting book with id: ' + req.params.id);
+  return BookModel.findById(req.params.id, function(err, book){
+    return book.remove(function(err){
+      if (!err){
+	console.log('Book removed');
+	return res.send('');
+      } else {
+	console.log(err);
+      }
+    });
+  });
 });
